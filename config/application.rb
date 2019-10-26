5105dfa6a902da5738860578ee1f391c624449cf
Muresan Beniamin <muresan.beniamin@hotmail.com>
2019-10-26 12:10:04 +0300

    finished authentication system"

--- config/application.rb
+++ config/application.rb
@@ -1,9 +1,7 @@
 require_relative 'boot'
-
 require 'rails/all'
+require_relative '../lib/api_errors/api_errors.rb'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
 module MediaTransformerToolV1
---
+++ lib/json_web_token.rb
@@ -0,0 +1,16 @@
+class JsonWebToken
+  class << self
+    def encode(payload, exp = 24.hours.from_now)
+      binding.pry
+      payload[:exp] = exp.to_i
+      JWT.encode(payload, Rails.application.credentials.content_path.read)
+    end
+ 
+    def decode(token)
+      body = JWT.decode(token, Rails.application.credentials.content_path.read)[0]
+      HashWithIndifferentAccess.new body
+    rescue
+      nil
+    end
+  end
+ end
