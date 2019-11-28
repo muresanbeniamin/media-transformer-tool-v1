b6f56cfaa3bb8dcaec3ae0fc360469c70638a0b6
Muresan Beniamin <muresan.beniamin@hotmail.com>
2019-11-28 12:40:52 +0200

    updated schema + migrations

--- db/schema.rb
+++ db/schema.rb
@@ -22,4 +22,12 @@ ActiveRecord::Schema.define(version: 2019_11_21_070457) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
 end
