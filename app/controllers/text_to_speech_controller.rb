b2b4a4cf9006baeba0b0d939fb7e5c4e633b74fc
Muresan Beniamin <muresan.beniamin@hotmail.com>
2019-11-28 11:51:16 +0200

    changes

--- app/controllers/text_to_speech_controller.rb
+++ app/controllers/text_to_speech_controller.rb
@@ -1,7 +1,10 @@
 class TextToSpeechController < ApplicationController
   def create
     text_to_speech_params[:text].to_file "en", "tmp/#{text_to_speech_params[:file_name]}.mp3"
+    render json: { message: 'success'}
   end
+
+  private
 
   def text_to_speech_params
     params.require(:text_to_speech).permit(:text, :file_name)
