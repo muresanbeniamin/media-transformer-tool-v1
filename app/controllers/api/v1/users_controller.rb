71e2ae16499eb4958e8f7d600ba628bf99830c9e
Muresan Beniamin <muresan.beniamin@hotmail.com>
2019-11-28 12:27:03 +0200

    changes

---
+++ app/controllers/api/v1/users_controller.rb
@@ -0,0 +1,30 @@
+module Api::V1
+  class UsersController < ApplicationController
+    skip_before_action :authenticate_request
+  
+    def signin
+      command = AuthenticateUser.call(params[:email], params[:password])
+  
+      if command.success?
+        render json: { auth_token: command.result }
+      else
+        render json: { error: command.errors }, status: :unauthorized
+      end
+    end
+
+    def signup
+      user = User.new(signup_params)
+      if user.save
+        render json: { auth_token: JsonWebToken.encode(user_id: user.id) }
+      else
+        render json: { error: user.errors.messages }, status: 422
+      end
+    end
+
+    private
+
+    def signup_params
+      params.permit(:email, :password)
+    end
+  end
+ end
